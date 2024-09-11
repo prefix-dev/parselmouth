d593f470830644ec02c696a0ce7a7ca8391463ef
nichmor <nmorkotilo@gmail.com>
2024-09-11 16:50:03 +0300

    misc: remove unused code

--- src/parselmouth/internals/remover.py
+++ src/parselmouth/internals/remover.py
@@ -1,8 +1,6 @@
 import asyncio
 import io
 import os
-from pathlib import Path
-import re
 from typing import Optional
 import aioboto3.session
 import botocore.client
@@ -10,14 +8,12 @@ from conda_forge_metadata.types import ArtifactData
 import concurrent.futures
 import logging
 from dotenv import load_dotenv
-from packaging.version import parse
 from parselmouth.internals.channels import BackendRequestType, SupportedChannels
 from parselmouth.internals.conda_forge import (
     get_all_packages_by_subdir,
     get_artifact_info,
 )
 from parselmouth.internals.s3 import IndexMapping
-from parselmouth.internals.utils import normalize
 
 import aioboto3
 
@@ -27,76 +23,6 @@ from parselmouth.internals.s3 import s3_client
 
 
 names_mapping: IndexMapping = IndexMapping.model_construct(root={})
-
-dist_info_pattern = r"([^/]+)-(\d+[^/]*)\.dist-info\/METADATA"
-egg_info_pattern = r"([^/]+?)-(\d+[^/]*)\.egg-info\/PKG-INFO"
-
-dist_pattern_compiled = re.compile(dist_info_pattern)
-egg_pattern_compiled = re.compile(egg_info_pattern)
-
-
-def check_if_is_direct_url(package_name: str, url: Optional[str]) -> bool:
-    if not url:
-        return False
-    urls = None
-    if not isinstance(url, str):
-        logging.warning(f"{package_name} contains multiple urls")
-        urls = url
-    else:
-        urls = list(url)
-
-    if all(
-        url.startswith("https://pypi.io/packages/")
-        or url.startswith("https://pypi.org/packages/")
-        or url.startswith("https://pypi.python.org/packages/")
-        for url in urls
-    ):
-        return False
-
-    return True
-
-
-def get_pypi_names_and_version(files: list[str]) -> dict[str, str]:
-    """
-    Return a dictionary of normalized names to it's version
-    """
-    package_names: dict[str, str] = {}
-    for file_name in files:
-        file_path = Path(file_name)
-        # sometimes, packages like setuptools have some stuff vendored
-        # that our regex will catch:
-        # site-packages/setuptools/_vendor/zipp-3.19.2.dist-info/RECORD
-        # but in reality we don't want to include itages:
-        if "_vendor" in file_path.parts or "_vendored" in file_path.parts:
-            continue
-        match = dist_pattern_compiled.search(file_name) or egg_pattern_compiled.search(
-            file_name
-        )
-        if match:
-            package_name = match.group(1)
-            if not package_name:
-                continue
-
-            version = match.group(2)
-            if "-py" in version:
-                index_of_py = version.index("-py")
-                version = version[:index_of_py]
-
-            pkg_version = None
-
-            try:
-                pkg_version = parse(version)
-            except Exception:
-                if "-" in version:
-                    index_of_dash = version.rfind("-")
-                    version = version[:index_of_dash]
-
-            if pkg_version:
-                version = str(pkg_version)
-
-            package_names[normalize(package_name)] = version
-
-    return package_names
 
 
 async def async_upload_package(
